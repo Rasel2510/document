3564f04611fa0d9a64bfa64b0a5498e18c2d1d41
Rasel Ahamed <rasel251062@gmail.com>
2026-03-30 15:43:57 +0600

    Add files via upload

---
+++ loading.dart
@@ -0,0 +1,20 @@
+import '../../../constants/common_imports.dart';
+
+class LoadingDialog {
+  static void show(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      barrierColor: Colors.black.withValues(alpha: 0.3),
+      builder: (_) => Center(
+        child: Lottie.asset(AppLotties.loading_animation, height: 100.h),
+      ),
+    );
+  }
+
+  static void hide(BuildContext context) {
+    if (Navigator.canPop(context)) {
+      Navigator.pop(context);
+    }
+  }
+}
